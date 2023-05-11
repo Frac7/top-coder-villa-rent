2a29144c7e9534d2328e6751f44983f5406c2b64
Francesca Cella <francescacella96@gmail.com>
2023-05-11 20:58:38 +0200

    Add list item component

---
+++ src/components/VillaItem.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { VillaItemProps } from "@/types";
+const props = defineProps<VillaItemProps>();
+</script>
+
+<template>
+  <div>
+    <img :src="props.image" />
+    <div>
+      <span>Location: {{ props.location }}</span>
+      <span>
+        Price:
+        {{
+          Intl.NumberFormat("it-IT", {
+            style: "currency",
+            currency: "EUR",
+          }).format(props.price)
+        }}
+      </span>
+      <span>
+        Capacity:
+        {{ props.capacity }}
+      </span>
+      <span>
+        Facilities:
+        {{ props.facilities }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,11 @@ export type SearchFormParams = {
   price?: number;
   capacity?: number;
 };
+
+export type VillaItemProps = {
+  image: string;
+  location: string;
+  price: number;
+  capacity: number;
+  facilities: string;
+};
